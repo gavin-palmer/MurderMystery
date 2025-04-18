fa39bc2e45268b749fff5276db911b638679891e
Gavin Palmer <gavin.palmer7@gmail.com>
2025-04-18 11:53:08 +0100

    Add unity project

--- MurderMystery/Generators/MysteryGenerator.cs
+++ MurderMystery/Generators/MysteryGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿
 using MurderMystery.Data;
 using MurderMystery.Enums;
 using MurderMystery.Helpers.MurderMystery.Utilities;
---
+++ MurderMysteryGame/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get horizontal and vertical input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement direction
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        // Apply movement
+        rb.velocity = movement * moveSpeed;
+    }
+}
